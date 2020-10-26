29b07aad9d88f8bccba899039efaa1bd0acaff0b
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2020-10-26 18:34:00 +0800

    Add form to cart view

--- app.rb
+++ app.rb
@@ -24,8 +24,8 @@ get '/products/:id' do
 end
 
 post '/cart' do
-	orders_input = params[:orders]
-	@items = parse_orders_input orders_input
+	@orders_input = params[:orders]
+	@items = parse_orders_input @orders_input
 
 	@items.each do |item|
 		item[0] = Product.find(item[0])
